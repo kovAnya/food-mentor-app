6c587084a7a6893057149622aef66d34d9aa9e8a
Anna Kovalchuk <annazukh@gmail.com>
2023-12-14 19:44:38 +0200

    progressBar: Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,15 @@ import { ProgressBar } from "./components/progressBar/ProgressBar";
 import { Layout } from "./components/layout/Layout";
 
 function App() {
-  const [progress, setProgress] = useState(2);
+  const [currentPage, setCurrentPage] = useState(0);
 
-  const onStepChange = (currentPage) => {
-    switch (currentPage) {
-      case 0:
-        setProgress(2);
-        break;
-      case 1:
-        setProgress(25);
-        break;
-      case 2:
-        setProgress(50);
-        break;
-      case 3:
-        setProgress(75);
-        break;
-      case 4:
-        setProgress(98);
-        break;
-      default:
-        setProgress(2);
-    }
+  const onStepChange = (page) => {
+    setCurrentPage(page);
   };
 
   return (
     <Layout>
-      <ProgressBar progress={progress} />
+      <ProgressBar currentPage={currentPage} />
       <MainForm onStepChange={onStepChange} />
     </Layout>
   );
