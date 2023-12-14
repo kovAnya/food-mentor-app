eca56d2f73299e7344f66987e29203ee40c1b459
Anna Kovalchuk <annazukh@gmail.com>
2023-12-14 07:11:47 +0200

    form: Third page styles

--- src/components/mainForm/thirdPage/ThirdPage.jsx
+++ src/components/mainForm/thirdPage/ThirdPage.jsx
@@ -1,6 +1,10 @@
 import css from "./ThirdPage.module.css";
 import { Formik, Field, Form } from "formik";
 
+import bodyTypeRegular from "../../../assets/quiz_img/body-type-1.png";
+import bodyTypePlumb from "../../../assets/quiz_img/body-type-2.png";
+import bodyTypeExtra from "../../../assets/quiz_img/body-type-3.png";
+
 export const ThirdPage = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,7 +17,7 @@ export const ThirdPage = (props) => {
 
   return (
     <div>
-      <h1>Choose your current body type</h1>
+      <h1 className={css.quiz_title}>Choose your current body type</h1>
       <Formik onSubmit={handleSubmit} initialValues={props.data}>
         <Form autoComplete="off">
           <div>
@@ -26,6 +30,9 @@ export const ThirdPage = (props) => {
                 onClick={handleChange}
               />
               Regular
+              <div className={`${css.img_thmb} ${css.color_secondary_main}`}>
+                <img className={css.img} src={bodyTypeRegular} alt="Regular" />
+              </div>
             </label>
             <label className={css.radio_label}>
               <Field
@@ -36,6 +43,9 @@ export const ThirdPage = (props) => {
                 onClick={handleChange}
               />
               Plumb
+              <div className={`${css.img_thmb} ${css.color_secondary_mono_1}`}>
+                <img className={css.img} src={bodyTypePlumb} alt="Plumb" />
+              </div>
             </label>
             <label className={css.radio_label}>
               <Field
@@ -46,6 +56,9 @@ export const ThirdPage = (props) => {
                 onClick={handleChange}
               />
               Extra Plumb
+              <div className={`${css.img_thmb} ${css.color_secondary_mono_3}`}>
+                <img className={css.img} src={bodyTypeExtra} alt="Extra" />
+              </div>
             </label>
           </div>
           <button
