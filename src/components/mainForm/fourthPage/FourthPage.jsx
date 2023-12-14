3130673985c6995a2105d7dd2090569a5e8a5cfd
Anna Kovalchuk <annazukh@gmail.com>
2023-12-14 07:55:53 +0200

    form: Fourth page styles

--- src/components/mainForm/fourthPage/FourthPage.jsx
+++ src/components/mainForm/fourthPage/FourthPage.jsx
@@ -1,6 +1,11 @@
 import css from "./FourthPage.module.css";
 import { Formik, Field, Form } from "formik";
 
+import workoutNot from "../../../assets/quiz_img/image-456.png";
+import workoutWalk from "../../../assets/quiz_img/image-840.png";
+import workoutTwoTimes from "../../../assets/quiz_img/image-879.png";
+import workoutFiveTimes from "../../../assets/quiz_img/image-1963.png";
+
 export const FourthPage = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,8 +17,8 @@ export const FourthPage = (props) => {
   };
 
   return (
-    <div>
-      <h1>Do you workout?</h1>
+    <>
+      <h1 className={css.quiz_title}>Do you workout?</h1>
       <Formik onSubmit={handleSubmit} initialValues={props.data}>
         <Form autoComplete="off">
           <div>
@@ -21,11 +26,14 @@ export const FourthPage = (props) => {
               <Field
                 className={css.radio_btn}
                 type="radio"
-                name="workoute"
+                name="workout"
                 value="No, I don't"
                 onClick={handleChange}
               />
               No, I don't
+              <div className={`${css.img_thmb} ${css.color_secondary_mono_1}`}>
+                <img className={css.img} src={workoutNot} alt="No, I don't" />
+              </div>
             </label>
             <label className={css.radio_label}>
               <Field
@@ -36,6 +44,9 @@ export const FourthPage = (props) => {
                 onClick={handleChange}
               />
               Only walks
+              <div className={`${css.img_thmb} ${css.color_secondary_mono_1}`}>
+                <img className={css.img} src={workoutWalk} alt="Only walks" />
+              </div>
             </label>
             <label className={css.radio_label}>
               <Field
@@ -46,6 +57,13 @@ export const FourthPage = (props) => {
                 onClick={handleChange}
               />
               1-2 times a week
+              <div className={`${css.img_thmb} ${css.color_secondary_mono_1}`}>
+                <img
+                  className={css.img}
+                  src={workoutTwoTimes}
+                  alt="1-2 times a week"
+                />
+              </div>
             </label>
             <label className={css.radio_label}>
               <Field
@@ -56,6 +74,13 @@ export const FourthPage = (props) => {
                 onClick={handleChange}
               />
               3-5 times a week
+              <div className={`${css.img_thmb} ${css.color_secondary_mono_1}`}>
+                <img
+                  className={css.img}
+                  src={workoutFiveTimes}
+                  alt="3-5 times a week"
+                />
+              </div>
             </label>
           </div>
           <button
@@ -68,6 +93,6 @@ export const FourthPage = (props) => {
           </button>
         </Form>
       </Formik>
-    </div>
+    </>
   );
 };
