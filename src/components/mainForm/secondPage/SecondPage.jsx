18f62de9df8671f50f2cc5ae475fdf670231c12d
Anna Kovalchuk <annazukh@gmail.com>
2023-12-14 06:22:15 +0200

    form: Second page styles

--- src/components/mainForm/secondPage/SecondPage.jsx
+++ src/components/mainForm/secondPage/SecondPage.jsx
@@ -1,6 +1,11 @@
 import css from "./SecondPage.module.css";
 import { Formik, Field, Form } from "formik";
 
+import goalShapeLoose from "../../../assets/quiz_img/goal-1.png";
+import goalShapePerfect from "../../../assets/quiz_img/goal-2.png";
+import goalShapeTone from "../../../assets/quiz_img/goal-3.png";
+import goalShapeHealthy from "../../../assets/quiz_img/goal-4.png";
+
 export const SecondPage = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,51 +17,89 @@ export const SecondPage = (props) => {
   };
 
   return (
-    <div>
-      <h1>Choose your goal</h1>
+    <>
+      <h1 className={css.quiz_title}>Choose your goal</h1>
       <Formik onSubmit={handleSubmit} initialValues={props.data}>
         <Form autoComplete="off">
-          <div>
-            <label className={css.radio_label}>
-              <Field
-                className={css.radio_btn}
-                type="radio"
-                name="goal"
-                value="Lose weight"
-                onClick={handleChange}
-              />
-              Lose weight
-            </label>
-            <label className={css.radio_label}>
-              <Field
-                className={css.radio_btn}
-                type="radio"
-                name="goal"
-                value="Get perfect fit body"
-                onClick={handleChange}
-              />
-              Get perfect fit body
-            </label>
-            <label className={css.radio_label}>
-              <Field
-                className={css.radio_btn}
-                type="radio"
-                name="goal"
-                value="Tone muscles"
-                onClick={handleChange}
-              />
-              Tone muscles
-            </label>
-            <label className={css.radio_label}>
-              <Field
-                className={css.radio_btn}
-                type="radio"
-                name="goal"
-                value="Be healthier"
-                onClick={handleChange}
-              />
-              Be healthier
-            </label>
+          <div className={css.radio_wrpr}>
+            <div className={css.radio_inner_wrpr}>
+              <label className={css.radio_label}>
+                <Field
+                  className={css.radio_btn}
+                  type="radio"
+                  name="goal"
+                  value="Lose weight"
+                  onClick={handleChange}
+                />
+                <div
+                  className={`${css.img_thmb} ${css.color_secondary_mono_1}`}
+                >
+                  <img
+                    className={css.img}
+                    src={goalShapeLoose}
+                    alt="Lose weight"
+                  />
+                </div>
+                Lose weight
+              </label>
+              <label className={css.radio_label}>
+                <Field
+                  className={css.radio_btn}
+                  type="radio"
+                  name="goal"
+                  value="Get perfect fit body"
+                  onClick={handleChange}
+                />
+                <div className={`${css.img_thmb} ${css.color_secondary_main}`}>
+                  <img
+                    className={css.img}
+                    src={goalShapePerfect}
+                    alt="Get perfect fit body"
+                  />
+                </div>
+                Get perfect fit body
+              </label>
+            </div>
+            <div className={css.radio_inner_wrpr}>
+              <label className={css.radio_label}>
+                <Field
+                  className={css.radio_btn}
+                  type="radio"
+                  name="goal"
+                  value="Tone muscles"
+                  onClick={handleChange}
+                />
+                <div
+                  className={`${css.img_thmb} ${css.color_secondary_mono_2}`}
+                >
+                  <img
+                    className={css.img}
+                    src={goalShapeTone}
+                    alt="Tone muscles"
+                  />
+                </div>
+                Tone muscles
+              </label>
+              <label className={css.radio_label}>
+                <Field
+                  className={css.radio_btn}
+                  type="radio"
+                  name="goal"
+                  value="Be healthier"
+                  onClick={handleChange}
+                />
+                <div
+                  className={`${css.img_thmb} ${css.color_secondary_mono_3}`}
+                >
+                  <img
+                    className={css.img}
+                    src={goalShapeHealthy}
+                    alt="Be healthier"
+                  />
+                </div>
+                Be healthier
+              </label>
+            </div>
           </div>
           <button
             onClick={() => props.prev()}
@@ -68,6 +111,6 @@ export const SecondPage = (props) => {
           </button>
         </Form>
       </Formik>
-    </div>
+    </>
   );
 };
